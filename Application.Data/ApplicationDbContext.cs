60a849459cc20737fa1c051593206062659b3fec
NikolayGeorgiev-hub <n.atanasov@itsoft.dev>
2023-10-19 12:47:14 +0300

    Fix some bugs, language service

--- Application.Data/ApplicationDbContext.cs
+++ Application.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Application.Data.Models.Categories;
+﻿using Application.Data.Models;
+using Application.Data.Models.Categories;
 using Application.Data.Models.Comments;
 using Application.Data.Models.Discounts;
 using Application.Data.Models.Orders;
@@ -7,6 +8,7 @@ using Application.Data.Models.Ratings;
 using Application.Data.Models.Users;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
 
 namespace Application.Data;
 
@@ -18,11 +20,26 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, Applicati
     }
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        builder
+            .Entity<Language>()
+            .HasKey(x => x.Id);
+
+        builder
+            .Entity<Language>()
+            .Property(x => x.Code)
+            .IsRequired()
+            .HasMaxLength(5);
+
+        builder
+            .Entity<Language>()
+            .Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(50);
         base.OnModelCreating(builder);
 
         this.ConfigureApplicationRelations(builder);
     }
-
+    public DbSet<Language> Languages { get; set; }
     public DbSet<Category> Categories { get; set; }
 
     public DbSet<SubCategory> SubCategories { get; set; }
